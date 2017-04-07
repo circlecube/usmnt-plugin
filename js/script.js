346a8a69420bb9e811f9f6c134d9113cc6952c66
Evan Mullins <emullins@brownbagmarketing.com>
2017-04-07 10:21:30 -0400

    adding existing files to new repo

---
+++ js/script.js
@@ -0,0 +1,272 @@
+jQuery(document).ready(function($) {
+
+	//match title generation
+	// date - opponent - type
+	
+	$('#publish').on('hover', function(e){
+		if ($("#title").val() == "") {
+			set_title();
+		}
+	});
+	
+	function set_title(){
+		var month = new Array();
+			month[0] = "January";
+			month[1] = "February";
+			month[2] = "March";
+			month[3] = "April";
+			month[4] = "May";
+			month[5] = "June";
+			month[6] = "July";
+			month[7] = "August";
+			month[8] = "September";
+			month[9] = "October";
+			month[10] = "November";
+			month[11] = "December";
+		
+		var date_val = $('#acf-field_54f28f849b8ed').val();
+		// var date = new Date( date_val.substring(0,4), date_val.substring(3,2), date_val.substring(5,2) );
+		var date_string = month[parseInt(date_val.substring(4,6))-1] + ' ' + date_val.substring(6,8) + ', ' + date_val.substring(0,4);
+		var opponent = $('#s2id_acf-field_54f2a27b9830c .select2-chosen').text();
+		var type = $('#s2id_acf-field_54f2a3cb13c7f .select2-chosen').text();
+		
+		$('#title').val( date_string + ' - ' + opponent + ' - ' + type );
+	}
+	//need the inverse, to read the title and populate the custom fields dynamically...
+	
+
+
+	$('#title').on('blur', function(e){
+		
+		var full_name = $(this).val();
+		full_name = full_name.split(' ');
+		$("#acf-field_54f48b2b39a94").val(full_name[0]);
+		$("#acf-field_54f48b3139a95").val(full_name[1]);
+		
+	});
+
+	var tax_opp_ids = [
+		{'id': '38', 'text': 'Algeria'}, {'id': '52', 'text': 'Antigua and Barbuda'}, {'id': '124', 'text': 'Arabia'}, {'id': '59', 'text': 'Argentina'}, {'id': '146', 'text': 'Armenia'}, {'id': '44', 'text': 'Australia'}, {'id': '43', 'text': 'Austria'}, {'id': '39', 'text': 'Azerbaijan'}, {'id': '68', 'text': 'Barbados'}, {'id': '35', 'text': 'Belgium'}, {'id': '50', 'text': 'Belize'}, {'id': '119', 'text': 'Bermuda'}, {'id': '136', 'text': 'Bolivia'}, {'id': '47', 'text': 'Bosnia-Herzegovina'}, {'id': '21', 'text': 'Brazil'}, {'id': '83', 'text': 'Cameroon'}, {'id': '23', 'text': 'Canada'}, {'id': '116', 'text': 'Cayman Islands'}, {'id': '31', 'text': 'Chile'}, {'id': '118', 'text': 'China'}, {'id': '67', 'text': 'China PR'}, {'id': '32', 'text': 'Colombia'}, {'id': '46', 'text': 'Costa Rica'}, {'id': '51', 'text': 'Cuba'}, {'id': '34', 'text': 'Czech Republic'}, {'id': '131', 'text': 'Czechoslovakia'}, {'id': '81', 'text': 'Denmark'}, {'id': '139', 'text': 'East Germany'}, {'id': '84', 'text': 'Ecuador'}, {'id': '74', 'text': 'Egypt'}, {'id': '48', 'text': 'El Salvador'}, {'id': '24', 'text': 'England'}, {'id': '127', 'text': 'Estonia'}, {'id': '128', 'text': 'Finland'}, {'id': '27', 'text': 'France'}, {'id': '26', 'text': 'Germany'}, {'id': '30', 'text': 'Ghana'}, {'id': '80', 'text': 'Grenada'}, {'id': '56', 'text': 'Guadeloupe'}, {'id': '49', 'text': 'Guatemala'}, {'id': '64', 'text': 'Haiti'}, {'id': '33', 'text': 'Honduras'}, {'id': '132', 'text': 'Hungary'}, {'id': '117', 'text': 'Iceland'}, {'id': '63', 'text': 'Iran'}, {'id': '115', 'text': 'Israel'}, {'id': '29', 'text': 'Italy'}, {'id': '129', 'text': 'Ivory Coast'}, {'id': '45', 'text': 'Jamaica'}, {'id': '78', 'text': 'Japan'}, {'id': '120', 'text': 'Kedah'}, {'id': '141', 'text': 'Kuwait'}, {'id': '76', 'text': 'Latvia'}, {'id': '148', 'text': 'Liechtenstein'}, {'id': '123', 'text': 'Luxembourg'}, {'id': '142', 'text': 'Macedonia'}, {'id': '135', 'text': 'Malaysia'}, {'id': '133', 'text': 'Malta'}, {'id': '82', 'text': 'Martinique'}, {'id': '22', 'text': 'Mexico'}, {'id': '147', 'text': 'Moldova'}, {'id': '77', 'text': 'Morocco'}, {'id': '62', 'text': 'Netherlands'}, {'id': '72', 'text': 'New Zealand'}, {'id': '37', 'text': 'Nigeria'}, {'id': '144', 'text': 'North Korea'}, {'id': '138', 'text': 'Northern Ireland'}, {'id': '79', 'text': 'Norway'}, {'id': '13', 'text': 'Panama'}, {'id': '58', 'text': 'Paraguay'}, {'id': '65', 'text': 'Peru'}, {'id': '61', 'text': 'Poland'}, {'id': '36', 'text': 'Portugal'}, {'id': '25', 'text': 'Republic of Ireland'}, {'id': '143', 'text': 'Romania'}, {'id': '53', 'text': 'Russia'}, {'id': '145', 'text': 'Saudi Arabia'}, {'id': '28', 'text': 'Scotland'}, {'id': '73', 'text': 'Slovakia'}, {'id': '55', 'text': 'Slovenia'}, {'id': '60', 'text': 'South Africa'}, {'id': '42', 'text': 'South Korea'}, {'id': '121', 'text': 'Soviet Union'}, {'id': '57', 'text': 'Spain'}, {'id': '137', 'text': 'Surinam'}, {'id': '75', 'text': 'Sweden'}, {'id': '126', 'text': 'Switzerland'}, {'id': '149', 'text': 'Thailand'}, {'id': '125', 'text': 'Tobago'}, {'id': '122', 'text': 'Trinidad'}, {'id': '130', 'text': 'Trinidad & Tobago'}, {'id': '66', 'text': 'Trinidad and Tobago'}, {'id': '69', 'text': 'Tunisia'}, {'id': '40', 'text': 'Turkey'}, {'id': '41', 'text': 'Ukraine'}, {'id': '70', 'text': 'Uruguay'}, {'id': '54', 'text': 'Venezuela'}, {'id': '71', 'text': 'Wales'}, {'id': '140', 'text': 'West Germany'}, {'id': '134', 'text': 'Yugoslavia'},
+	];
+	var tax_type_ids = [
+		{'id': '20', 'text': 'Confederations Cup'},
+		{'id': '12', 'text': 'Friendly'},
+		{'id': '19', 'text': 'Gold Cup'},
+		{'id': '16', 'text': 'Olympics'},
+		{'id': '17', 'text': 'World Cup'},
+		{'id': '18', 'text': 'World Cup Qualifier'},
+		{'id': '109', 'text': 'Kirin Cup'},
+		{'id': '108', 'text': 'Carlsberg Cup'},
+		{'id': '107', 'text': 'Joe Robbie Cup'},
+		{'id': '113', 'text': 'Olympic Qualifier'},
+		{'id': '112', 'text': 'President’s Cup'},
+		{'id': '111', 'text': 'Miami Cup'},
+		{'id': '114', 'text': 'Mexico City Tournament'},
+		{'id': '110', 'text': 'Copa América'},
+	];
+	var tax_result_ids = [
+		{'id': '11', 'text': 'W'},
+		{'id': '14', 'text': 'L'},
+		{'id': '15', 'text': 'D'}
+	];
+
+	
+	$('body.post-type-match #poststuff #title').on('focus', function(e){
+		
+		if ( $(this).val() != '' ){
+			//May 30, 2012 – Brazil – Friendly
+			var meta = $(this).val().split(' - ');
+			
+			//date
+			if( $('.hasDatepicker').val() == '') {
+				console.log(meta[0]);
+				// 	$('.hasDatepicker').trigger('focus');
+				// 	$('.hasDatepicker').val(meta[0]);
+				// 	$('.hasDatepicker.active').trigger('keydown');
+				// 	$('.hasDatepicker.active').trigger('blur');
+				// 	$('.hasDatepicker.active').removeClass('active');
+				$(".hasDatepicker").datepicker("setDate", new Date(meta[0]));
+			}
+			
+			if ( $('#new-tag-opponent').val() == '' ){
+				//opponent
+				var opp_id;
+				// for (var this_type in tax_type_ids){
+				for(var i=0;i<tax_opp_ids.length;i++){
+					this_opp = tax_opp_ids[i];
+					
+					// console.log(this_opp['id'], this_opp['text']);
+					if (this_opp['text'] == meta[1]) {
+						opp_id = this_opp['id'];
+					}
+				}
+				console.log(opp_id, meta[1]);
+				// $('#s2id_acf-field_54f2a27b9830c .select2-choice').trigger('mousedown');
+				$("#acf-field_54f2a27b9830c").select2("data", {id: opp_id, text: meta[1]}); 
+				// $('#new-tag-opponent').val(meta[1]);
+			}
+	
+			//type
+			if ( $('#new-tag-match-type').val() == '' ){
+				var type_id;
+				// console.log(meta[2]);
+				// for (var this_type in tax_type_ids){
+				for(var i=0;i<tax_type_ids.length;i++){
+					this_type = tax_type_ids[i];
+					
+					// console.log(this_type['id'], this_type['text']);
+					if (this_type['text'] == meta[2]) {
+						type_id = this_type['id'];
+					}
+				}
+				console.log(type_id, meta[2]);
+				$("#acf-field_54f2a3cb13c7f").select2("data", {id: type_id, text: meta[2]});
+				// $('#new-tag-match-type').val(meta[2]);
+			}
+			
+			//result
+			if( $('new-tag-result').val() == '' && meta[3] ){
+				var result_id;
+				for(var i=0;i<tax_result_ids.length;i++){
+					this_result = tax_result_ids[i];
+					
+					// console.log(this_type['id'], this_type['text']);
+					if (this_result['text'] == meta[3]) {
+						result_id = this_result['id'];
+					}
+				}
+				console.log(result_id, meta[3]);
+				$("#acf-field_54f2a3a013c7e").select2("data", {id: result_id, text: meta[3]});
+			}
+		}
+	});
+	
+
+
+
+
+
+	//on load if title not empty
+	if ( $('body.post-type-match #poststuff #title').length > 0 &&
+		 $('body.post-type-match #poststuff #title').val() != '' && 
+		 $('body.post-type-match #poststuff #title').val().indexOf('  ') != -1 ) {
+		
+		//November 17, 2010  W  1-0  South Africa      
+		var meta = $("#title").val().split('  ');
+		//0 November 17, 2010  
+		//1 W
+		//2 1-0 
+		//3 South Africa
+		// console.log( meta[0], meta[1], meta[2], meta[3]);
+		//date
+		if( $('.hasDatepicker').val() == '') {
+			// console.log(meta[0]);
+			$(".hasDatepicker").datepicker("setDate", new Date(meta[0]));
+		}
+		
+		//result
+		if( $('#new-tag-result').val() == '' && meta[1] ){
+			var result_id;
+			for(var i=0; i < tax_result_ids.length; i++){
+				this_result = tax_result_ids[i];
+				
+				// console.log(this_result['id'], this_result['text']);
+				if (this_result['text'] == meta[1]) {
+					result_id = this_result['id'];
+				}
+			}
+			// console.log(result_id, meta[1]);
+			$("#acf-field_54f2a3a013c7e").select2("data", {id: result_id, text: meta[1]});
+		}
+		
+		//score
+		if( $('#acf-field_54f290a4487f9').val() == '') {
+			$('#acf-field_54f290a4487f9').val( meta[2] );
+		}
+		
+		//opponent
+		if ( $('#new-tag-opponent').val() == '' ){
+			var opp_id;
+			// for (var this_type in tax_type_ids){
+			for(var i=0; i < tax_opp_ids.length; i++){
+				this_opp = tax_opp_ids[i];
+				
+				// console.log(this_opp['id'], this_opp['text']);
+				if (this_opp['text'] == meta[3]) {
+					opp_id = this_opp['id'];
+				}
+			}
+			// console.log(opp_id, meta[3]);
+			// $('#s2id_acf-field_54f2a27b9830c .select2-choice').trigger('mousedown');
+			$("#acf-field_54f2a27b9830c").select2("data", {id: opp_id, text: meta[3]}); 
+			// $('#new-tag-opponent').val(meta[1]);
+		}
+		
+		var notes = $('#acf-field_54f5c1e6ee5e2').val();
+		if ( 	  notes.indexOf('(WCQ') > -1 ){
+				meta[4] = 'World Cup Qualifier';
+		}
+		else if ( notes.indexOf('(WCQ') > -1 ){
+				meta[4] = 'World Cup';
+		}
+		else if ( notes.indexOf('(GC') > -1 ){
+				meta[4] = 'Gold Cup';
+		}
+		else if ( notes.indexOf('(G.C.') > -1 ){
+				meta[4] = 'Gold Cup';
+		}
+		else if ( notes.indexOf('(Gold') > -1 ){
+				meta[4] = 'Gold Cup';
+		}
+		else if ( notes.indexOf('(OLY') > -1 ){
+				meta[4] = 'Olympics';
+		}
+		else if ( notes.indexOf('(OLQ') > -1 ){
+				meta[4] = 'Olympic Qualifier';
+		}
+		else if ( notes.indexOf('(CC') > -1 ){
+				meta[4] = 'Confederations Cup';
+		}
+		else if ( notes.indexOf('(Conf') > -1 ){
+				meta[4] = 'Confederations Cup';
+		}
+		else if ( notes.indexOf('(Kirin') > -1 ){
+				meta[4] = 'Kirin Cup';
+		}
+		else if ( notes.indexOf('(Joe') > -1 ){
+				meta[4] = 'Joe Robbie Cup';
+		}
+		else if ( notes.indexOf('(Carlsberg') > -1 ){
+				meta[4] = 'Carlsberg Cup';
+		}
+		else if ( notes.indexOf('(CA’') > -1 ){
+				meta[4] = 'Copa América';
+		}
+		else if ( notes.indexOf('(MCT') > -1 ){
+				meta[4] = 'Mexico City Tournament';
+		}
+		else {
+				meta[4] = 'Friendly';
+		}
+		var type_id;
+		// for (var this_type in tax_type_ids){
+		for(var i=0; i < tax_type_ids.length; i++){
+			this_type = tax_type_ids[i];
+			
+			// console.log(this_type['id'], this_type['text']);
+			if (this_type['text'] == meta[4]) {
+				type_id = this_type['id'];
+			}
+		}
+		// console.log(type_id, meta[4]);
+		$("#acf-field_54f2a3cb13c7f").select2("data", {id: type_id, text: meta[4]});
+		
+		
+		
+		$("#title").val(meta[0] + ' - ' + meta[3] + ' - ' + meta[4]);
+		$("#publish").trigger('click');
+	}
+		
+	
+	
+});
---
+++ usmnt.php
@@ -0,0 +1,313 @@
+<?php
+/**
+ * @package USMNT
+ * @version 1.0
+ */
+/*
+Plugin Name: USMNT Functionality Plugin
+Plugin URI: http://app.circlcube.com/usmnt
+Description: Functionality for USMNT App
+Author: Evan Mullins
+Version: 1.0
+*/
+
+
+	
+	function publish_match_update_players( $ID, $post ) {
+		delete_transient( 'roster_json' );
+		delete_transient( 'all_matches_table' );
+		delete_transient( 'all_coaches_table' );
+
+		// increment caps, matches and goals for players when they are added to a match
+		if ( $post->post_type == 'match') {
+			
+			//check players array for player ids
+			$players = get_field('players');
+			foreach($players as $player) {
+				
+				//but only when the update players checkbox is ticked.
+				if ( get_field('update_players_on_save') ) {
+					//increment caps for that player
+					$current_player_caps = intval(get_field('caps', $player)) + 1;
+					//field_54f28167cd525
+					update_field('field_54f28167cd525', $current_player_caps, $player);
+				}
+				//add match to players list of matches - ALWAYS
+				//field_54f296d36fa8b
+				$current_player_matches = get_field('matches', $player);
+				if ( is_array($current_player_matches) ) {
+					array_push($current_player_matches, $post->ID);
+				}
+				else {
+					$current_player_matches[0] = $post->ID;
+				}
+				update_field('field_54f296d36fa8b', $current_player_matches, $player);
+			}
+			
+			//check goals repeater for player ids
+			if ( get_field('update_players_on_save') ){
+				if ( have_rows('goals') ) {
+					while( have_rows('goals') ) {
+						the_row();
+						$goalers = get_sub_field('player');
+						if ( $goalers ) {
+							foreach ( $goalers as $goaler ) {
+								// setup_postdata($post);
+								$goaler_id = $goaler;
+								//increment goals for that goalers
+								$current_goaler_goals = intval(get_field('goals', $goaler_id)) + 1;
+								//field_54f2817dcd526
+								update_field('field_54f2817dcd526', $current_goaler_goals, $goaler_id);
+							}
+							// wp_reset_postdata();
+						}
+					}
+				}
+			}
+			//update notes field for this match - just for quick testing
+			// update_field('field_54f5c1e6ee5e2', 'new save ' . $current_goaler_goal . ' ' . $goaler_id);
+			
+			//check coach and update their stats too!
+			$coachs = get_field('coach');
+			foreach($coachs as $coach) {
+				
+				//but only when the update players checkbox is ticked.
+				if ( get_field('update_players_on_save') ) {
+					//increment caps for that coach
+					$current_coach_caps = intval( get_field('caps', $coach) ) + 1;
+					//field_559d4d607f03b
+					update_field('field_559d4d607f03b', $current_coach_caps, $coach);
+					
+					//increment win/lose/draw value depending on if the match was a win/lose/draw
+					$result_terms = get_the_terms( $post->ID, 'result');
+					$results = [];
+					foreach ( $result_terms as $term ) {
+						$results[] = $term->name;
+						// var_dump($term);
+						$result_permalink = get_term_link( $term );
+					}
+					$result = $results[0];
+					// update_field('field_54f5c1e6ee5e2', 'new save ' . $result);
+					if ( $result == 'W' ) {
+						$current_coach_wins = intval( get_field('wins', $coach) ) + 1;
+						update_field('field_559d4d607f0ba', $current_coach_wins, $coach);
+					}
+					if ( $result == 'L' ){
+						$current_coach_loses = intval( get_field('loses', $coach) ) + 1;
+						update_field('field_559d4da90ee58', $current_coach_loses, $coach);
+					}
+					if ( $result == 'D' ){
+						$current_coach_draws = intval( get_field('draws', $coach) ) + 1;
+						update_field('field_559d4eed2148c', $current_coach_draws, $coach);
+					}
+					
+				}
+				
+				//add match to coachs list of matches - ALWAYS
+				//field_559d4d607f22c
+				$current_coach_matches = get_field('matches', $coach);
+				if ( is_array($current_coach_matches) ) {
+					array_push($current_coach_matches, $post->ID);
+				}
+				else {
+					$current_coach_matches[0] = $post->ID;
+				}
+				update_field('field_559d4d607f22c', $current_coach_matches, $coach);
+			}
+			
+			if ( get_field('update_players_on_save') ) {
+				//set update option to false
+				update_field('field_54f9b03e48e42', false);
+			}
+			
+		}
+		
+		// add player to match when match is added to player
+		if ( $post->post_type == 'player' ) {
+			
+			//check match relationship field data
+			$player_matches = get_field('matches');
+			if ( $player_matches ) {
+				//loop all matches and 
+				foreach( $player_matches as $match) {
+					//add player to match
+					$current_match_players = get_field('players', $match);
+					if ( is_array($current_match_players) ){
+						if ( !in_array($post->ID, $current_match_players) ) {
+							array_push($current_match_players, $post->ID);
+						}
+					}
+					else {
+						$current_match_players[0] = $post->ID;
+					}
+					//field_54f290379b8f3
+					update_field('field_54f290379b8f3', $current_match_players, $match);
+					// update_field('field_54f5c1e6ee5e2', 'updated from player', $match);
+
+				}
+			}
+			
+		}
+		
+		// add coach to match when match is added to coach
+		if ( $post->post_type == 'coach' ) {
+			
+			//check match relationship field data
+			$coach_matches = get_field('matches');
+			if ( $coach_matches ) {
+				//loop all matches and 
+				foreach( $coach_matches as $match) {
+					//add coach to match
+					$current_match_coaches = get_field('coach', $match);
+					if ( $current_match_coaches ){
+						// if data already exists do nothing
+					}
+					else {
+						$current_match_coaches[0] = $post->ID;
+					}
+					//field_55a50a8e38597
+					update_field('field_55a50a8e38597', $current_match_coaches, $match);
+					// update_field('field_54f5c1e6ee5e2', 'updated from player', $match);
+
+				}
+			}
+			
+		}
+	}
+	add_action( 'save_post', 'publish_match_update_players', 10, 3 );
+	
+
+	// api call for full list of players alphabetized
+	// http://music.circlecube.com/wp-json/posts?type=song&filter[posts_per_page]=-1&filter[orderby]=title&filter[order]=ASC
+
+	// api call for full list of matches
+	// http://app.circlecube.com/usmnt/wp-json/posts?type=match&filter[posts_per_page]=-1
+
+	//https://github.com/WP-API/WP-API/issues/433
+	//add ACF fields to json
+	function json_api_prepare_post( $post_response, $post, $context ) {
+	  if( get_fields($post['ID']) ){
+	  	// add ACF fileds into an acf object
+	    $post_response['acf'] = get_fields($post['ID']);
+	    // add ACF fields at root of post object
+	    // $acf_fields = get_fields($post['ID']);
+    	// foreach ($acf_fields as $key => $value) {
+    	//     $post_response[$key] = $value;
+    	// }
+    	
+    	
+	    return $post_response;
+	  }
+	}
+	// add_filter( 'json_prepare_post', 'json_api_prepare_post', 10, 3 );
+
+
+
+	//  load custom js for data entry helper
+	// Register Script
+	function usmnt_scripts() {
+		// if( $hook != 'edit.php' ) 
+				// return;
+		 
+			wp_enqueue_script( 'usmnt_js', plugins_url( 'usmnt/js/script.js' , dirname(__FILE__) ) );
+			
+	}
+	// Hook into the 'admin_enqueue_scripts' action
+	add_action( 'admin_enqueue_scripts', 'usmnt_scripts' );
+	
+	//load custom css for admin style tweaks
+	function usmnt_styles() {
+		wp_enqueue_style( 'usmnt_css', plugins_url( 'usmnt/css/styles.css', dirname(__FILE__) ) );
+	}
+	add_action( 'admin_enqueue_scripts', 'usmnt_styles' );
+
+
+
+	//https://gist.github.com/klihelp/fb4f292a1ec20e97efab
+	
+	
+	/**
+	 * Posts per page
+	 */
+	function kli_wp_api_query_post_per_page( $query ){
+	    if ( defined( 'JSON_REQUEST' ) && TRUE == JSON_REQUEST ){
+	            $query->set( 'posts_per_page', -1);
+	    }
+	}
+	add_action( 'pre_get_posts', 'kli_wp_api_query_post_per_page' );
+
+
+
+	/**
+	 * Prepare post fields - Remove some fields
+	 * @param  object $data   
+	 * @return object
+	 */
+	function kli_wp_api_prepare_post( $data ){
+
+	    // var_dump($data);
+	    // exit;
+
+	    // Remove fields
+	    $fields = [ 'author',
+	    			'status',
+	    			'excerpt',
+	    			'guid',
+	    			'parent',
+	    			'format',
+	    			'meta',
+	    			'menu_order',
+	    			'date_tz',
+	    			'modified_tz',
+	    			'modified_gmt',
+					'comment_status',
+					'ping_status',
+					'sticky',
+					'date_gmt'
+				];
+	    
+	    foreach ($fields as $value) {
+	        if ( isset($data[$value]) ) {
+	            unset($data[$value]);
+	        }
+	    }
+
+	    return $data;
+	}
+	add_action( 'json_prepare_post', 'kli_wp_api_prepare_post', 100);
+
+
+
+
+
+	function array_sort_by_column(&$array, $column, $direction = SORT_ASC) {
+	    $reference_array = array();
+
+	    foreach($array as $key => $row) {
+	        $reference_array[$key] = $row[$column];
+	    }
+
+	    array_multisort($reference_array, $direction, $array);
+	}
+	
+	
+	// sort the matches by date - NOT WORKING
+	//http://www.advancedcustomfields.com/resources/customize-the-relationship-field-list-query/
+	function match_acf_result_query( $args, $field, $post ) {
+	    // eg from https://codex.wordpress.org/Class_Reference/WP_Query#Custom_Field_Parameters
+	    $args['meta_query'] = array(
+	        array(
+				'meta_key'		=> 'date',
+				'orderby'		=> 'meta_value_num',
+				'order'			=> 'DESC'
+	        )
+	    );
+
+	    return $args;
+	}
+
+	// acf/fields/relationship/result/name={$field_name} - filter for a specific field based on it's name
+	// add_filter('acf/fields/relationship/queryname=matches', 'match_acf_result_query', 10, 3);
+	
+	
+?>
